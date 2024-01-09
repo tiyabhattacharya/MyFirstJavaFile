63e4e3fcc324eb8d7a4febba39e81553e4c48700
tiyabhattacharya <211b328@juetguna.in>
2024-01-09 13:28:55 +0530

    chapter1

--- src/Variables.java
+++ src/Variables.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Variables {
     public static void main(String[] args) {
         
@@ -9,6 +11,36 @@ public class Variables {
         System.out.println(sum);
 
         //ques2
+        float m1=68;
+        float m2=68;
+        float m3=68;
+        float cgpa=(m1+m2+m3)/30;
+        System.out.println(cgpa);
+
+        //ques3
+        System.out.println("enter your name");
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("hello"+" "+name);
         
+
+        //ques 4
+        System.out.println("Give the distance in km");
+        float km=sc.nextFloat();
+        float miles=0.62f*km;
+        System.out.println(miles);
+
+        //ques5
+        System.out.println("Enter a number");
+        int ab=sc.nextInt();
+        if(ab==(int)ab){
+            System.out.println("It is an integer");
+        }
+        else{
+            System.out.println("Not an integer");
+        }
+        sc.close();
+
+
     }
 }
