c2733ae45b58f8df57b4e113f3e6a44664b129bb
tiyabhattacharya <211b328@juetguna.in>
2024-01-09 13:16:09 +0530

    Java

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        //ques1
+        int a=70;
+        int b=30;
+        int c=23;
+        int sum=a+b+c;
+        System.out.println(sum);
+    }
+}
