e982582e10407ab2fd0d9e98f8abd2c9ef1157b3
tiyabhattacharya <211b328@juetguna.in>
2024-01-18 12:40:42 +0530

    changes

--- src/Constructors.java
+++ src/Constructors.java
@@ -1,10 +1,14 @@
 class Rect{
     private int l;
     private int b;
-    public Rect(int length,int breadth){
-         l=length;
-         b=breadth;
+    
+    public Rect(int l, int b) {
+        this.l = l;
+        this.b = b;
     }
+    
+    
+    
     public void details(){
         System.out.println("The area of rectangle is: "+ l*b);
         System.out.println("Perimeter of rectangle is: "+ 2*(l+b));
@@ -14,7 +18,7 @@ class Rect{
 
 public class Constructors {
     public static void main(String[] args) {
-        Rect r=new Rect(6,4);
+        Rect r=new Rect(3,4);
         r.details();
 
     }
