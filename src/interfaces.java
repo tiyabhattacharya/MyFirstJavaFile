4897d4634a0e2f3d6332baede97663ba17e8a1dc
tiyabhattacharya <211b328@juetguna.in>
2024-01-18 23:08:39 +0530

    interfaces

---
+++ src/interfaces.java
@@ -0,0 +1,14 @@
+interface tvremote{
+    void on();
+    void off();
+}
+interface smartremote extends tvremote{
+     void control();
+     void smartcontrol();
+}
+
+public class interfaces {
+    public static void main(String[] args) {
+        
+    }
+}
