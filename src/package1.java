a3db49120613fd4838a7362442427911f56efd9a
tiyabhattacharya <211b328@juetguna.in>
2024-02-01 14:12:15 +0530

    packages

---
+++ src/package1.java
@@ -0,0 +1,26 @@
+package Calc;
+
+class Calculatorrr{
+    public void calculate(int a, int b){
+        System.out.println("Your result is "+ (a+b));
+    }
+}
+
+class ScCalculatorrr{
+    public void calculate(int a, int b){
+        System.out.println("Your result is "+ Math.sin(a+b));
+    }
+}
+
+class HyCalculatorrr{
+    public void calculate(int a, int b){
+        System.out.println("Your result is "+ (a^b));
+    }
+}
+
+
+public class package1 {
+    public static void main(String[] args) {
+        System.out.println("I am main method.");
+    }
+}
---
+++ src/package2.java
@@ -0,0 +1,27 @@
+//package folder.folder1.folder2;
+
+class Calculatorrr1{
+    public void calculate(int a, int b){
+        System.out.println("Your result is "+ (a+b));
+    }
+}
+
+class ScCalculatorrr1{
+    public void calculate(int a, int b){
+        System.out.println("Your result is "+ Math.sin(a+b));
+    }
+}
+
+class HyCalculatorrr1{
+    public void calculate(int a, int b){
+        System.out.println("Your result is "+ (a^b));
+    }
+}
+
+
+public class package2 {
+    public static void main(String[] args) {
+        System.out.println("I am main method.");
+    }
+}
+
